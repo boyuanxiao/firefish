bd7f05fc63236d24ceff2eefcd87a163ea3e12bc
boyuanxiao <bx215@cam.ac.uk>
2016-06-28 22:01:52 +0100

    parse viscous forces

--- Martlet3/parseForces.py
+++ Martlet3/parseForces.py
@@ -16,18 +16,23 @@ rhoNorm = 1.4
 rhoReal = 1.13
 vNorm = 1.1 
 vReal = 345
+muNorm = 5.45e-08
+muReal = 1.8e-05
 scaling = -(rhoReal/rhoNorm) * (vReal/vNorm)**2
+scalingViscous = -(muReal)/(muNorm) * (vReal/vNorm)
 
 #setup arrays
 t = []
 fpx = []; fpy = []; fpz = []; #Pressure
-fpx1 = []; fpy1 = []; fpz1 = []; #Pressure                                                                                                                                                                                                                                                                                                                                                                                              
+fpx1 = []; fpy1 = []; fpz1 = []; #Pressure  
+fvx = []; fvy = []; fvz = []; #Pressure                                                                                                                                                                                                                                                                                                                                                                                              
+fvx1 = []; fvy1 = []; fvz1 = []; #Pressure                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 mpx = []; mpy = []; mpz = []; #Moment Pressure                                                                                                                                                                                        
 mpx1 = []; mpy1 = []; mpz1 = []; #Moment Pressure                                                                                                                                                                                                                                                                                                                                                                                
 
 #read the .dat files
-pipefileDart=open('../Martlet3/stabilityResults/postProcessing/forces1/0/forces_2e-05.dat','r')
-pipefileCore=open('../Martlet3/separationResults/postProcessing/core/0.095/forces.dat','r')
+pipefileDart=open('../Martlet3/turbulentM1p1A5/postProcessing/forcesCore/0/forces.dat','r')
+pipefileCore=open('../Martlet3/turbulentM1p1A5/postProcessing/forcesDart/0/forces.dat','r')
 
 linesDart = pipefileDart.readlines()
 linesCore = pipefileCore.readlines()
@@ -42,6 +47,9 @@ for i in xrange(0, lth, lth/200):
 		fpx.append(float(match.group(2)))
 		fpy.append(float(match.group(3)))
 		fpz.append(float(match.group(4)))
+		fvx.append(float(match.group(5)))
+		fvy.append(float(match.group(6)))
+		fvz.append(float(match.group(7)))
 		mpx.append(float(match.group(11)))
 		mpy.append(float(match.group(12)))
 		mpz.append(float(match.group(13)))				   
@@ -51,6 +59,9 @@ for i in xrange(0, lth, lth/200):
 		fpx1.append(float(match1.group(2)))
 		fpy1.append(float(match1.group(3)))
 		fpz1.append(float(match1.group(4)))
+		fvx1.append(float(match.group(5)))
+		fvy1.append(float(match.group(6)))
+		fvz1.append(float(match.group(7)))
 		mpx1.append(float(match.group(11)))
 		mpy1.append(float(match.group(12)))
 		mpz1.append(float(match.group(13)))
@@ -59,20 +70,24 @@ for i in xrange(0, lth, lth/200):
 fpx = [x * scaling for x in fpx]
 fpz = [x * scaling for x in fpz]
 fpy = [x * scaling for x in fpy]
+fvx = [x * scalingViscous for x in fvx]
+fvy = [x * scalingViscous for x in fvy]
+fvz = [x * scalingViscous for x in fvz]
 mpx = [x * scaling for x in mpx]
 mpy = [x * scaling for x in mpy]
 mpz = [x * scaling for x in mpz]
 
 fpy1 = [x * scaling for x in fpy1]
+fvy1 = [x * scalingViscous for x in fvy1]
 sep = numpy.subtract(fpy1, fpy)
 
 #plot the forces
 plt.figure()
 plt.xlabel('Simulation time (sec)')
-plt.ylabel('Restoring moment (Nm)')
-#plt.plot(t, mpx,'g', label = "Moment x axis")
-#plt.plot(t, mpy,'b', label = "Moment y axis")
-plt.plot(t, mpz, 'c', label = "Restoring moment")
+plt.ylabel('Forces (N)')
+plt.plot(t, fpy,'g', label = "Force Pressure core y")
+plt.plot(t, fpy1,'b', label = "Force Pressure dart y")
+#plt.plot(t, mpz, 'c', label = "Restoring moment")
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.05),
 		  ncol=3, fancybox=True, shadow=True)
 output = "restoringMoment.pdf"
